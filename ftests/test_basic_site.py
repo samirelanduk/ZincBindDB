9e9167f49a5843d9f26719c6703938305a2db552
Sam Ireland <sam.ireland.uk@gmail.com>
2017-09-15 16:21:00 +0100

    Add nav CSS

--- ftests/test_basic_site.py
+++ ftests/test_basic_site.py
@@ -29,3 +29,24 @@ class BasePageLayoutTests(FunctionalTest):
             header = links.find_element_by_class_name("footer-header")
             self.assertGreater(len(links.find_elements_by_tag_name("a")), 2)
         copyright = footer.find_element_by_id("copyright")
+
+
+    def test_basic_page_css(self):
+        self.get("/")
+
+        # The nav is correct
+        nav = self.browser.find_element_by_tag_name("nav")
+        self.assertGreater(nav.size["height"], 50)
+        self.assertNotEqual(
+         nav.value_of_css_property("background-color"), "rgba(0, 0, 0, 0)"
+        )
+
+        # The nav links are horizontally aranged
+        nav_links = nav.find_element_by_id("nav-links")
+        nav_links = nav_links.find_elements_by_tag_name("li")
+        for index, link in enumerate(nav_links):
+            self.assertEqual(link.location["y"], nav_links[0].location["y"])
+            if index:
+                self.assertGreater(
+                 link.location["x"], nav_links[index - 1].location["x"]
+                )
