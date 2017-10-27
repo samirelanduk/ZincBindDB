ae1679cc24d56b317891b3324499ce9333eba834
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-27 19:59:48 +0100

    Add Residue model

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@ from mixer.backend.django import mixer
 from django.core.exceptions import ValidationError
 from unittest.mock import patch, Mock, MagicMock
 from .base import ZincBindTest
-from zincbind.models import Pdb
+from zincbind.models import Pdb, Residue
 
 class PdbTests(ZincBindTest):
 
@@ -13,10 +13,6 @@ class PdbTests(ZincBindTest):
          resolution=4.5, checked="2017-01-01"
         )
         pdb.full_clean()
-        pdb.save()
-        self.assertEqual(Pdb.objects.all().count(), 1)
-        retrieved_pdb = Pdb.objects.first()
-        self.assertEqual(retrieved_pdb, pdb)
 
 
     def test_title_is_required(self):
@@ -62,3 +58,88 @@ class PdbTests(ZincBindTest):
          pk="1XXY", title="The PDB Title", deposited="1990-09-28",
          resolution=4.5, checked=None
         )
+        pdb.full_clean()
+
+
+
+class ResidueTests(ZincBindTest):
+
+    def setUp(self):
+        ZincBindTest.setUp(self)
+        self.pdb = mixer.blend(Pdb)
+
+
+    def test_can_create_residue(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain="A", number=10,
+         pdb=self.pdb
+        )
+        residue.full_clean()
+
+
+    def test_residueid_is_required(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id=None, name="VAL", chain="A", number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+        residue = Residue(
+         pk="1XYZA10", residue_id="", name="VAL", chain="A", number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+
+
+    def test_name_is_required(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name=None, chain="A", number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="", chain="A", number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+
+
+    def test_chain_is_required(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain=None, number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain="", number=10,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+
+
+    def test_number_is_required(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain="A", number=None,
+         pdb=self.pdb
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain="A", number=0,
+         pdb=self.pdb
+        )
+        residue.full_clean()
+
+
+    def test_pdb_is_required(self):
+        residue = Residue(
+         pk="1XYZA10", residue_id="A10", name="VAL", chain="A", number=10,
+         pdb=None
+        )
+        with self.assertRaises(ValidationError):
+            residue.full_clean()
--- zincbind/models.py
+++ zincbind/models.py
@@ -1,9 +1,27 @@
+"""ZincBind models."""
+
 from django.db import models
 
 class Pdb(models.Model):
+    """Represents a PDB structure file. There should be one for every PDB file
+    ther is."""
 
-    id = models.TextField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=64)
     title = models.TextField()
     deposited = models.DateField()
     resolution = models.FloatField()
     checked = models.DateField(blank=True, null=True)
+
+
+
+class Residue(models.Model):
+    """Represents a residue in the PDB sense - that is it can be an amino acid
+    residue in a macromolecular chain, or it can be a small molecule such as
+    water."""
+
+    id = models.TextField(primary_key=True)
+    residue_id = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+    chain = models.CharField(max_length=16)
+    number = models.IntegerField()
+    pdb = models.ForeignKey(Pdb)
