214a2b9f6e2b6c959ea6e5b832efc0261bb9428f
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-27 18:16:44 +0100

    Restart 0.1

--- zincsites/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from zincsites import views
-
-urlpatterns = [
- url(r"^new/$", views.new_site_page, name="new_site_page"),
- url(r"^(.+?)/edit/$", views.edit_site_page, name="edit_site_page"),
- url(r"^(.+?)/$", views.site_page, name="site_page"),
- url(r"^$", views.sites_page, name="sites_page"),
-]
