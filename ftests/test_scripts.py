8708a3b0ae075e6b37aa5b1b1e2fd5cfcbfe29a4
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-30 14:29:28 +0000

    Start testing of scripts

--- ftests/test_scripts.py
+++ ftests/test_scripts.py
@@ -1,6 +1,16 @@
+from unittest.mock import patch
 from .base import FunctionalTest
+from zincbind.models import *
+from scripts.add_pdbs import main
 
 class AddingScriptTests(FunctionalTest):
 
-    def test_successful_add(self):
-        pass
+    @patch("scripts.add_pdbs.get_all_pdb_codes")
+    @patch("builtins.print")
+    def test_successful_add(self, mock_print, mock_add):
+        mock_add.return_value = [
+         "1AAA", "1AAB", "1AAC", "1AAD", "2AAA", "2AAB", "2AAC", "2AAD",
+         "2SAM", "1LOL", "1TON"
+        ]
+        main()
+        self.assertEqual(len(Pdb.objects.all()), 11)
--- scripts/add_pdbs.py
+++ scripts/add_pdbs.py
@@ -11,29 +11,33 @@ from zincbind.exceptions import RcsbError, AtomiumError
 from zincbind.factories import create_empty_pdb, create_zinc_site
 
 
-print("Fetching PDB codes...")
-pdb_codes = get_all_pdb_codes()
-print("There are {} current PDB codes.".format(len(pdb_codes)))
+def main():
+    print("Fetching PDB codes...")
+    pdb_codes = get_all_pdb_codes()
+    print("There are {} current PDB codes.".format(len(pdb_codes)))
 
-remove_checked_pdbs(pdb_codes)
-print("There are {} which have never been checked.".format(len(pdb_codes)))
+    remove_checked_pdbs(pdb_codes)
+    print("There are {} which have never been checked.".format(len(pdb_codes)))
 
-print("Checking PDBs...")
-for code in pdb_codes:
-    try:
-        print("\tChecking {}...".format(code))
-        if zinc_in_pdb(code):
-            print("\tFound Zinc")
-            pdb = get_pdb(code)
-            model = pdb.model()
-            for zinc in model.molecules(name="ZN"):
-                site = zinc.site()
-                create_zinc_site(pdb, zinc, site.residues())
-                print("\t\tAdded {}".format(site))
-        else:
-            print("\tNo Zinc")
-            create_empty_pdb(code)
-    except RcsbError:
-        print("Problem with {} RSCB".format(code))
+    print("Checking PDBs...")
+    for code in pdb_codes:
+        try:
+            print("\tChecking {}...".format(code))
+            if zinc_in_pdb(code):
+                print("\tFound Zinc")
+                pdb = get_pdb(code)
+                model = pdb.model()
+                for zinc in model.molecules(name="ZN"):
+                    site = zinc.site()
+                    create_zinc_site(pdb, zinc, site.residues())
+                    print("\t\tAdded {}".format(site))
+            else:
+                print("\tNo Zinc")
+                create_empty_pdb(code)
+        except RcsbError:
+            print("Problem with {} RSCB".format(code))
 
-    print("")
+        print("")
+
+if __name__ == "__main__":
+    main()
