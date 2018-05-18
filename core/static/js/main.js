3b3b620437a60f6115f1d05226b0f649cc50107c
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-18 01:02:59 +0100

    Format species names

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -19,3 +19,16 @@ $(document).ready(function() {
         }
     });
 });
+
+
+function formatSpecies(species) {
+    var words = species.toLowerCase().split(" ");
+    words[0] = words[0][0].toUpperCase() + words[0].slice(1);
+    var regex = /\d/g;
+    for (var i = 0; i < words.length; i++) {
+        if (regex.test(words[i])) {
+            words[i] = words[i].toUpperCase();
+        }
+    }
+    return words.join(" ")
+}
