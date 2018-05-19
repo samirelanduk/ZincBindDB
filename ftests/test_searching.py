24484043594aa0a59fd00fbd35bc6fa7d89eef15
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-19 18:14:51 +0100

    Add handling of no results

--- ftests/test_searching.py
+++ ftests/test_searching.py
@@ -2,14 +2,38 @@ from .base import FunctionalTest
 
 class QuickSearchTests(FunctionalTest):
 
-    def test_can_search_pdb_codes(self):
-        # User searches for PDB code
-        self.get("/")
+    def quick_search(self, term):
         search = self.browser.find_element_by_id("site-search")
         search_form = search.find_element_by_tag_name("form")
         inputs = search_form.find_elements_by_tag_name("input")
-        inputs[0].send_keys("a001")
+        inputs[0].send_keys(term)
         self.click(inputs[1])
+
+
+    def test_basic_search_no_results(self):
+        # User searches for something that has no results
+        self.get("/")
+        self.quick_search("PlibblePlebblePlunk")
+
+        # They are on the search page
+        self.check_page("/search?q=PlibblePlebblePlunk")
+        self.check_title("Search Results: PlibblePlebblePlunk")
+
+        # The search information is there
+        info = self.browser.find_element_by_class_name("search-info")
+        self.assertIn("no results", info.text)
+        self.assertIn("PDB codes", info.text)
+        self.assertIn("PDB descriptions", info.text)
+
+        # There are no results
+        results = self.browser.find_elements_by_class_name("pdb-result")
+        self.assertEqual(len(results), 0)
+
+
+    def test_can_search_pdb_codes(self):
+        # User searches for PDB code
+        self.get("/")
+        self.quick_search("a001")
 
         # They are on the search page
         self.check_page("/search?q=a001")
