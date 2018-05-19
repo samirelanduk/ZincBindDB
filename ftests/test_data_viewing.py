5cc20e0230858d00ad535b1ea87ceb2566f7715d
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-19 23:04:52 +0100

    Remake PDB page

--- ftests/test_data_viewing.py
+++ ftests/test_data_viewing.py
@@ -8,17 +8,13 @@ class PdbPageTests(FunctionalTest):
         self.check_title("A001")
         self.check_h1("A SUPERB PDB FILE")
 
-        '''# There is information about the PDB
-        pdb_information = self.browser.find_element_by_id("pdb-information")
-        self.assertIn("A SUPERB PDB FILE", pdb_information.text)
-        self.assertIn("Deposited: 28 September, 1990", pdb_information.text)
+        # There is information about the PDB
+        pdb_information = self.browser.find_element_by_id("pdb-info")
+        self.assertIn("Deposition Date: 28 September, 1990", pdb_information.text)
         self.assertIn("Classification: REDUCTASE", pdb_information.text)
         self.assertIn("Technique: X-RAY", pdb_information.text)
         self.assertIn("Source Organism: Felis catus", pdb_information.text)
         self.assertIn("Expression System: Escherichia coli BL21(DE3)", pdb_information.text)
-        self.assertIn("Resolution: 2.1", pdb_information.text)
-        self.assertIn("R-factor: 4.5", pdb_information.text)
-        self.assertIn("PDB Code: A001", pdb_information.text)
-
-        # There is an NGL 3D display of the PDB
-        pdb_display = self.browser.find_element_by_id("ngl-display")'''
+        self.assertIn("Resolution: 2.1 Å", pdb_information.text)
+        self.assertIn("R-factor: 4.5 Å", pdb_information.text)
+        self.assertIn("Code: A001", pdb_information.text)
