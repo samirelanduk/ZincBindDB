0a4cace280854a87bbd6c3e65598639c5f0afd33
Sam Ireland <sam.ireland.uk@gmail.com>
2018-06-10 13:42:52 +0100

    Add data page

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 import core.views as core_views
 import zinc.views as zinc_views
 
 urlpatterns = [
  path(r"search", zinc_views.search),
  path(r"search/", zinc_views.search),
+ path(r"data/", core_views.data),
  path(r"changelog/", core_views.changelog),
  path(r"pdbs/<slug:code>/", zinc_views.pdb),
  path(r"<slug:pk>/", zinc_views.zinc_site),
