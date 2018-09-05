4a6e7d9213cf9393d121eabc04eea65e784a005a
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-05 13:08:51 +0100

    Start 0.6

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = binascii.hexlify(os.urandom(24)).decode()
 
-VERSION = "0.5.1"
+VERSION = "0.6.0"
 
 ALLOWED_HOSTS = []
 
