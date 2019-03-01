6f012b4cc998f857a3f484f103927e6a54e268f2
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-01 16:00:51 +0000

    Basic prediction system working

--- scripts/make_profiles.py
+++ scripts/make_profiles.py
@@ -9,6 +9,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 import django; django.setup()
 from core.models import *
 from tqdm import tqdm
+from itertools import combinations
+import numpy as np
+import matplotlib.pyplot as plt
 
 FAMILIES = ["E2"]
 
@@ -18,3 +21,40 @@ for family in FAMILIES:
     # Get all relevant binding sites
     sites = ZincSite.objects.filter(family=family, pdb__resolution__lt=2.5)
     print(f"  There are {sites.count()} sites to process.")
+
+    profiles = []
+    for site in sites:
+        # Only use residues on a chain, not ligands
+        residues = Residue.objects.filter(site=site).exclude(chain=None)
+
+        # Create combinations
+        combos = list(combinations(residues, 2))
+
+        # Make profile
+        profile = []
+        for combo in combos:
+            res1, res2 = combo
+            profile.append(round(
+             res1.atom_set.get(name="CA").distance_to(res2.atom_set.get(name="CA")), 3
+            ))
+            profile.append(round(
+             res1.atom_set.get(name="CB").distance_to(res2.atom_set.get(name="CB")), 3
+            ))
+        profiles.append(profile)
+
+    # Distribution
+    bins = sites.count() / 10
+    alphas, betas = zip(*profiles)
+    hist, x, y = np.histogram2d(alphas, betas, bins=bins)
+    average = lambda c: sum(c) / len(c)
+    average_count = average([average(row) for row in hist])
+    #print(average_count)
+    #print(hist)
+    hist = [[round(-np.log(val / average_count), 3) for val in row] for row in hist]
+    #print(hist)
+    plt.imshow(hist, interpolation='nearest', origin='low', extent=[x[0], x[-1], y[0], y[-1]])
+    #plt.show()
+
+    data = f"{hist}\n\n{list(x)}\n\n{list(y)}".replace("inf", "np.inf")
+    with open(f"data/profiles/{family}.dat", "w") as f:
+        f.write(data)
--- scripts/predict.py
+++ scripts/predict.py
@@ -11,6 +11,7 @@ import django; django.setup()
 from django.conf import settings
 from itertools import combinations
 import atomium
+import numpy as np
 
 def print_and_log(log_path, string):
     with open(log_path, "a") as f:
@@ -31,36 +32,68 @@ log_path = f"{folder}/output.txt"
 # Start log
 print_and_log(log_path, "Starting job...")
 
-# Open file
-print_and_log(log_path, f"Opening {sys.argv[1].split('/')[-1]}...")
-model = atomium.open(sys.argv[1]).model
-print_and_log(log_path, f"Loaded {sys.argv[1].split('/')[-1]} - {len(model.atoms())} atoms")
+try:
+    # Open file
+    print_and_log(log_path, f"Opening {sys.argv[1].split('/')[-1]}...")
+    model = atomium.open(sys.argv[1]).model
+    print_and_log(log_path, f"Loaded {sys.argv[1].split('/')[-1]} - {len(model.atoms())} atoms")
 
-# What profiles are there?
-profiles = [f for f in os.listdir(settings.PROFILE_PATH) if f.endswith(".dat")]
+    # What profiles are there?
+    profiles = [f for f in os.listdir(settings.PROFILE_PATH) if f.endswith(".dat")]
 
-# Use each profile in turn
-for profile in profiles:
-    print_and_log(log_path, f"Looking for {profile[:-4]} sites...")
+    # Use each profile in turn
+    for profile in profiles:
+        print_and_log(log_path, f"Looking for {profile[:-4]} sites...")
 
-    # Get residue combinations
-    residues = []
-    residue = []
-    for char in profile:
-        residue.append(char)
-        if char.isdigit():
-            residue[-1] = int(residue[-1])
-            residues.append(residue)
-            residue = []
-    all_combos = []
-    for residue in residues:
-        all_residues = model.residues(code=residue[0])
-        combos = list(combinations(all_residues, residue[1]))
-        all_combos.append(combos)
-    while len(all_combos) > 1:
-         all_combos.insert(0, [(x,y) for x in all_combos[0] for y in all_combos[1]])
-         all_combos.pop(1)
-         all_combos.pop(1)
-    all_combos = all_combos[0]
-    if all_combos and isinstance(all_combos[0][0], tuple): all_combos = [r[0] for r in all_combos]
-    print_and_log(log_path, f"\t{len(all_combos)} possible combinations...")
+        # Get residue combinations
+        residues = []
+        residue = []
+        for char in profile:
+            residue.append(char)
+            if char.isdigit():
+                residue[-1] = int(residue[-1])
+                residues.append(residue)
+                residue = []
+        all_combos = []
+        for residue in residues:
+            all_residues = model.residues(code=residue[0])
+            print_and_log(log_path, f"\tThere are {len(all_residues)} {residue[0]} residues")
+            combos = list(combinations(all_residues, residue[1]))
+            all_combos.append(combos)
+        while len(all_combos) > 1:
+             all_combos.insert(0, [(x,y) for x in all_combos[0] for y in all_combos[1]])
+             all_combos.pop(1)
+             all_combos.pop(1)
+        all_combos = all_combos[0]
+        if all_combos and isinstance(all_combos[0][0], tuple): all_combos = [r[0] for r in all_combos]
+        print_and_log(log_path, f"\t{len(all_combos)} possible combinations...")
+
+        # Create vectors
+        vectors = {}
+        print_and_log(log_path, "\tCreating vectors...")
+        for combo in all_combos:
+            res1, res2 = combo
+            dist1 = round(res1.atom(name="CA").distance_to(res2.atom(name="CA")), 3)
+            dist2 = round(res1.atom(name="CB").distance_to(res2.atom(name="CB")), 3)
+            vectors[combo] = [dist1, dist2]
+
+        # Load energy landscape
+        with open(settings.PROFILE_PATH + "/" + profile) as f:
+            lines = f.read().splitlines()
+            grid = eval(lines[0])
+            X = eval(lines[2])
+            Y = eval(lines[4])
+
+        # Evaluate potential sites in turn
+        scores = {}
+        for combo, vector in vectors.items():
+            for x in range(len(X) - 1):
+                for y in range(len(Y) - 1):
+                    if X[x] < vector[0] < X[x + 1] and Y[y] < vector[1] < Y[y + 1]:
+                        scores[combo] = grid[x][y]
+                        break
+            if combo not in scores: scores[combo] = np.inf
+        for combo, score in sorted(scores.items(), key=lambda s: s[1]):
+            print_and_log(log_path, f"{str(combo[0])[9:-1]}, {str(combo[1])[9:-1]} {score} {vectors[combo]}")
+except Exception as e:
+    print_and_log(log_path, str(e))
