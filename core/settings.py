5aa0c956b043597fdeefa870f29c56dc870ad22e
Sam Ireland <sam.ireland.uk@gmail.com>
2019-06-02 11:40:02 +0100

    Setup Django

---
+++ core/settings.py
@@ -0,0 +1,25 @@
+import os
+from .secrets import SECRET_KEY, BASE_DIR, DATABASES
+
+ALLOWED_HOSTS = []
+
+DEBUG = True
+
+ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "graphene_django",
+ "core"
+]
+
+DATE_FORMAT = "D j M, Y"
+USE_TZ = True
+TIME_ZONE = "UTC"
+
+MIDDLEWARE = [
+ "django.middleware.common.CommonMiddleware",
+]
+
+STATIC_URL = "/static/"
