aacadaa437b271316fc949995235f1f35d96af46
Sam Ireland <sam.ireland.uk@gmail.com>
2019-07-07 18:13:52 +0100

    Use rsync instead of scp

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,8 @@ host="api.zincbind.net"
 # Empty the current source directory on the server
 ssh $host "rm -r ~/$host/source/* >& /dev/null"
 
-# Work out which files to send
-files="`git ls-files`"
-
-# Make server directory structure
-commands=()
-for f in $files; do
-    dir=`dirname $f`
-    commands+=("mkdir -p ~/$host/source/$dir;")
-done
-ssh $host "${commands[@]}"
-
-# Copy files to server one by one
-for f in $files; do
-    scp $f $host:~/$host/source/$f
-done
+# Send git tracked files
+rsync -vr . --exclude-from='.gitignore' --exclude='.git' $host:~/$host/source
 
 # Copy secrets
 scp core/secrets.py $host:~/$host/source/core/secrets.py
