f10f0131dafbb8001c0ff379c96cb800b2211f9a
Sam Ireland <sam.ireland.uk@gmail.com>
2021-01-21 02:10:38 +0000

    Default port

--- manage.py
+++ manage.py
@@ -3,5 +3,7 @@ import os
 import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+from django.core.management.commands.runserver import Command as runserver
+runserver.default_port = "8050"
 from django.core.management import execute_from_command_line
 execute_from_command_line(sys.argv)
